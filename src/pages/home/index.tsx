9df16a47762b8c8fee1520e9c54fee2288a83a72
lvy <upwardsrwr@gmail.com>
2024-11-24 22:27:22 +0800

    feat: init

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { type Model } from '@antv/x6'
-import {  useEffect, useRef, useState } from 'react'
-import {FlowChart} from '@/components'
+import { useEffect, useRef, useState } from 'react'
+import { FlowChart } from '@/components'
 // import SvgData from '@/components/svg/test.svg'
 const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="888" height="483.61099" viewBox="0 0 888 483.61099" xmlns:xlink="http://www.w3.org/1999/xlink">
      <rect id="node1" x="10" y="10" width="100" height="40" />
@@ -8,7 +8,6 @@ const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="888" height="4
 </svg>
 
 `
-
 
 const data = {
   nodes: [
@@ -73,66 +72,57 @@ const data = {
   ]
 }
 
-
-
-
-
-
 function Home() {
   const refContainer = useRef<HTMLDivElement>(null)
 
   const [graphData, setGraphData] = useState<Model.FromJSONData>()
 
+  const svgToAnt = (svg: string) => {
+    // 使用 DOMParser 解析 SVG
+    const parser = new DOMParser()
+    const svgDoc = parser.parseFromString(svgString, 'image/svg+xml')
+    const elements = svgDoc.documentElement.childNodes
 
+    // 转换为 X6 节点格式
+    const x6Nodes = Array.from(elements)
+      .map((node) => {
+        // 确保 node 是一个 Element
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          const element = node as SVGElement // 类型断言为 SVGElement
 
-
-    const svgToAnt = (svg: string) => {
-      // 使用 DOMParser 解析 SVG
-      const parser = new DOMParser()
-      const svgDoc = parser.parseFromString(svgString, 'image/svg+xml')
-      const elements = svgDoc.documentElement.childNodes
-
-      // 转换为 X6 节点格式
-      const x6Nodes = Array.from(elements)
-        .map((node) => {
-          // 确保 node 是一个 Element
-          if (node.nodeType === Node.ELEMENT_NODE) {
-            const element = node as SVGElement // 类型断言为 SVGElement
-
-            const nodeData = {
-              id: element.getAttribute('id'),
-              x: parseFloat(element.getAttribute('x') || element.getAttribute('cx') || '0'),
-              y: parseFloat(element.getAttribute('y') || element.getAttribute('cy') || '0'),
-              width: 300,
-              height: 200,
-              label: element.tagName === 'rect' ? '矩形' : '圆形', // 根据元素类型设置标签
-              shape: element.tagName === 'rect' ? 'rect' : 'circle',
-            }
-
-            return nodeData
+          const nodeData = {
+            id: element.getAttribute('id'),
+            x: parseFloat(element.getAttribute('x') || element.getAttribute('cx') || '0'),
+            y: parseFloat(element.getAttribute('y') || element.getAttribute('cy') || '0'),
+            width: 300,
+            height: 200,
+            label: element.tagName === 'rect' ? '矩形' : '圆形', // 根据元素类型设置标签
+            shape: element.tagName === 'rect' ? 'rect' : 'circle'
           }
-          return null // 过滤非元素节点
-        })
-        .filter((node) => node !== null) // 过滤掉 null
 
-      return x6Nodes
-    }
+          return nodeData
+        }
+        return null // 过滤非元素节点
+      })
+      .filter((node) => node !== null) // 过滤掉 null
 
-    useEffect(() => {
-      console.log('svgString', svgString)
-      const svgData1 = svgToAnt(svgString)
-      setGraphData({
-        nodes: svgData1,
-      } as Model.FromJSONData)
-      console.log(svgData1)
-    },[])
+    return x6Nodes
+  }
 
-
+  useEffect(() => {
+    console.log('svgString', svgString)
+    const svgData1 = svgToAnt(svgString)
+    setGraphData({
+      nodes: svgData1
+    } as Model.FromJSONData)
+    console.log(svgData1)
+  }, [])
 
   return (
     <div className="w-screen h-screen">
-      {graphData?.toString()}
-      <FlowChart data={graphData} />
+      1111
+      {/* {graphData?.toString()} */}
+      {/* <FlowChart data={graphData} /> */}
     </div>
   )
 }
