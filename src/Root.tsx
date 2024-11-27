195ebd53c5031c3bc704582434a4ad0a4a86da3d
lvy <upwardsrwr@gmail.com>
2024-11-27 14:49:29 +0800

    fix: icon

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,8 +4,6 @@ import { useEffect } from 'react'
 
 nprogress.configure({ showSpinner: false })
 
-
-
 /**
  * 生成页面标题
  * @description
@@ -14,20 +12,19 @@ nprogress.configure({ showSpinner: false })
  */
 const getDocumentTitle = (title?: string) => (title ? `${title} ` : 'flow-chart')
 
-
 export default function Root() {
   const navigation = useNavigation()
   const location = useLocation()
 
   // 监听路由变化，显示进度条
   useEffect(() => {
+    console.log(navigation.state)
     if (navigation.state === 'loading') {
       nprogress.start()
     } else {
       nprogress.done()
     }
   }, [navigation.state])
-
 
   useEffect(() => {
     document.title = getDocumentTitle('flow-chart')
